92ca72d6abf6aa621e7c77dc3840327edfb718fb
Matheus Sanada <maatheusdavid@me.com>
2025-07-06 21:55:52 -0300

    adjusrt url

--- SilverguardCAM/Sources/SilverguardCAM/Service/Common/BaseURL.swift
+++ SilverguardCAM/Sources/SilverguardCAM/Service/Common/BaseURL.swift
@@ -1,5 +1,5 @@
 enum BaseURL: String {
-    case debug = "http://localhost:3001"
+    case debug = "https://test.camapi.sosgolpe.com.br/"
     case staging = "https://staging.camapi.sosgolpe.com.br"
     case production = "https://camapi.sosgolpe.com.br"
 }
